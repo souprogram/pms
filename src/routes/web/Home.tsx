032a3d8442f3889fee59471526307ac6f9b13445
Alan Bubalo <alan.buba5@gmail.com>
2025-04-18 22:40:13 +0200

    optimize using infinite query

--- src/routes/web/Home.tsx
+++ src/routes/web/Home.tsx
@@ -1,41 +1,70 @@
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import React from "react";
+import { Button } from "../../components/ui/button";
 import { BlogCard } from "../../components/web/blog/blog-card";
 import { supabase } from "../../database/supabase";
 import { Blog } from "../../types/blog";
 
 export function Home() {
-  const blogsQuery = useQuery({
+  const {
+    data,
+    isPending,
+    isError,
+    fetchNextPage,
+    hasNextPage,
+    isFetching,
+    isFetchingNextPage,
+  } = useInfiniteQuery({
     queryKey: ["blog_list"],
-    queryFn: async () => {
-      const { data, error } = await supabase
+    queryFn: async ({ pageParam }) => {
+      const { data, error, count } = await supabase
         .from("blogs")
-        .select("*")
-        .order("created_at", { ascending: false });
+        .select("*", { count: "exact" })
+        .range(pageParam * 10, (pageParam + 1) * 10 - 1);
 
       if (error) {
         console.error("Error fetching blogs:", error);
         throw error;
       }
 
-      return data as Blog[];
+      const hasNextPage = (pageParam + 1) * 10 < (count ?? 0);
+
+      return {
+        blogs: data as Blog[],
+        nextCursor: hasNextPage ? pageParam + 1 : undefined,
+      };
     },
+    initialPageParam: 0,
+    getNextPageParam: (lastPage) => lastPage.nextCursor,
   });
 
-  if (blogsQuery.isPending) {
+  if (isPending) {
     return <div>Loading...</div>;
   }
 
-  if (blogsQuery.isError) {
+  if (isError) {
     return <div>Something went wrong</div>;
   }
 
   return (
     <>
       <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-        {blogsQuery.data.map((blog) => (
-          <BlogCard key={blog.id} blog={blog} />
+        {data.pages.map((group, i) => (
+          <React.Fragment key={i}>
+            {group.blogs.map((blog) => (
+              <BlogCard key={blog.id} blog={blog} />
+            ))}
+          </React.Fragment>
         ))}
       </div>
+
+      <div>{isFetching && !isFetchingNextPage ? "Fetching..." : null}</div>
+
+      {hasNextPage && (
+        <Button onClick={() => fetchNextPage()} disabled={isFetchingNextPage}>
+          {isFetchingNextPage ? "Loading more..." : "Load More"}
+        </Button>
+      )}
     </>
   );
 }
